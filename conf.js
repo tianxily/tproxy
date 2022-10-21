13f71633aedb01902417a768f815e6db9dd81356
Tianxi田溪 <96676423+tianxily@users.noreply.github.com>
2022-10-21 11:00:10 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '115',
+  ver: '120',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -19,18 +19,10 @@ jsproxy_config({
       }
     },
     // 该节点用于加载大体积的静态资源
-    'cfworker': {
-      label: 'CF加速节点',
+    'cfworker1': {
+      label: '机速节点',
       lines: {
-        // 收费版（高权重）
-        //'node-cfworker-2.etherdream.com': 4,
-
-        // 免费版（低权重，分摊一些成本）
-        // 每个账号每天 10 万次免费请求，但有频率限制
-        'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        'b.lulu.workers.dev': 1,
-        'b.jsproxy.workers.dev': 1,
+        'oracleusa.ml': 1,
       }
     }
   },
@@ -39,12 +31,11 @@ jsproxy_config({
    * 默认节点
    */
   node_default: 'mysite',
-  // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
    */
-  node_acc: 'cfworker',
+  node_acc: 'mysite',
 
   /**
    * 静态资源 CDN 地址
